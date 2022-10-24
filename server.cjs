322d7997973bb3881060b1556f521a7924af93bd
Ayon Jodder <ayonjodder177@gmail.com>
2022-10-24 22:30:17 +0600

    Exploring URL

--- server.cjs
+++ server.cjs
@@ -1,17 +1,28 @@
 const http = require('http');
 const PORT = 5000;
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('Welcome to the Node server');
-        res.end();
-    }
-    if (req.url === '/about') {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.write(JSON.stringify({ result: 'success', message: 'Welcome to the about page' }));
-        res.end();
-    }
+
+    //Exploring URL in Node...
+
+    // const address_url = 'http://localhost:5000/contact?name=ayon&city=khulna';
+    // const parsedUrl = url.parse(address_url, true)
+    // console.log(parsedUrl.query);
+
+
+    // Making server and send response url based...
+
+    // if (req.url === '/') {
+    //     res.writeHead(200, { 'Content-Type': 'text/html' });
+    //     res.write('Welcome to the Node server');
+    //     res.end();
+    // }
+    // if (req.url === '/about') {
+    //     res.writeHead(200, { 'Content-Type': 'application/json' });
+    //     res.write(JSON.stringify({ result: 'success', message: 'Welcome to the about page' }));
+    //     res.end();
+    // }
 })
 
 server.listen(PORT, () => {
