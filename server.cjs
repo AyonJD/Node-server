6888f4051c2b106c8f755fbed581e7bfb26a800e
Ayon Jodder <ayonjodder177@gmail.com>
2022-10-24 22:47:46 +0600

    Exploring file system

--- server.cjs
+++ server.cjs
@@ -1,6 +1,7 @@
 const http = require('http');
 const PORT = 5000;
 const url = require('url');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 
@@ -22,6 +23,56 @@ const server = http.createServer((req, res) => {
     //     res.writeHead(200, { 'Content-Type': 'application/json' });
     //     res.write(JSON.stringify({ result: 'success', message: 'Welcome to the about page' }));
     //     res.end();
+    // }
+
+
+
+    //Exploring file system in Node...
+
+    //Reading data from file in asyncronusly...
+    // if (req.url === '/') {
+    //     fs.readFile('data.txt', (err, data) => {
+    //         if (err) {
+    //             res.write('Failed to read file');
+    //             res.end();
+    //         } else {
+    //             res.write(data);
+    //             res.end();
+    //         }
+    //     })
+    // }
+
+    //Reading data from file in syncronusly...
+    // if (req.url === '/') {
+    //     const data = fs.readFileSync('data.txt');
+    //     res.write(data);
+    //     res.end();
+    // }
+
+    //Writing data to file in asyncronusly...
+    // if (req.url === '/') {
+    //     fs.writeFile('data.txt', 'New data...........', (err) => {
+    //         if (err) {
+    //             res.write('Failed to update file');
+    //             res.end();
+    //         } else {
+    //             res.write('New data updated successfully');
+    //             res.end();
+    //         }
+    //     })
+    // }
+
+    //Writing data to a new file asyncronusly...
+    // if (req.url === '/') {
+    //     fs.writeFile('newData.txt', 'New data...........', (err) => {
+    //         if (err) {
+    //             res.write('Failed to update file');
+    //             res.end();
+    //         } else {
+    //             res.write('New data updated successfully');
+    //             res.end();
+    //         }
+    //     })
     // }
 })
 
